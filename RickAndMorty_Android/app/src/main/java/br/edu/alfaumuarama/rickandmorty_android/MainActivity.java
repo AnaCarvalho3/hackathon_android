f3f19bdbc549dd7961a4600c4d671f151f0d8272
AnaCarvalho3 <anacarvalho3197@gmail.com>
2022-11-28 01:51:38 -0300

    AppManager

--- RickAndMorty_Android/app/src/main/java/br/edu/alfaumuarama/rickandmorty_android/MainActivity.java
+++ RickAndMorty_Android/app/src/main/java/br/edu/alfaumuarama/rickandmorty_android/MainActivity.java
@@ -2,7 +2,10 @@ package br.edu.alfaumuarama.rickandmorty_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button characterButton = findViewById(R.id.characters_button);
+        Button episodesButton = findViewById(R.id.episodes_button);
+        characterButton.setOnClickListener(v -> OpenActivity("character"));
+        episodesButton.setOnClickListener(v -> OpenActivity("episode"));
+
+
+    }
+    public void OpenActivity(String value) {
+        Intent intent = new Intent(this, ListActivity.class);
+        intent.putExtra("type", value);
+        startActivity(intent);
     }
 }
